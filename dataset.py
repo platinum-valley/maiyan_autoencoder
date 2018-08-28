9d17bd0064008c11156990b2efe29316073c6ae6
platinum-valley <s163351@edu.tut.ac.jp>
2018-08-28 23:07:50 +0900

    first commit

---
+++ dataset.py
@@ -0,0 +1,14 @@
+import torch
+import torchvision
+
+
+class Dataset(torch.utils.data.Dataset):
+
+    def __init__(self):
+        pass
+
+    def __len__(self):
+        pass
+
+    def __getitem__(self):
+        pass
