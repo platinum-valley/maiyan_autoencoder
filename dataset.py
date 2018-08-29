d530781b5ee6f2b620cb3ec28748361ed4536a2c
platinum-valley <s163351@edu.tut.ac.jp>
2018-08-29 22:38:58 +0900

    update gitignore

--- dataset.py
+++ dataset.py
@@ -15,27 +15,26 @@ def imread(filename, flags=cv2.IMREAD_COLOR, dtype=np.uint8):
         return None
 
 
-def face_detection(image_file):
-    if not os.path.exists(image_file):
-        print("not found image file")
-        return None
+def face_detection(image_dir, output_dir):
     cascade_path = "./haarcascade_frontalface_default.xml"
-
-    image = imread(image_file)
-    image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    cascade = cv2.CascadeClassifier(cascade_path)
-
-    facerect = cascade.detectMultiScale(image_gray, scaleFactor=1.1, minNeighbors=2, minSize=(30, 30))
-
     color = (255, 255, 255)
-    if len(facerect) > 0:
+    image_shape = (255, 255)
+    image_list = os.listdir(image_dir)
+    count_image = 0
+    for image_file in image_list:
+        image_path = image_dir + "/" + image_file
+        image = imread(image_path)
+        image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        cascade = cv2.CascadeClassifier(cascade_path)
 
-        for rect in facerect:
-            cv2.rectangle(image, tuple(rect[0:2]), tuple(rect[0:2] + rect[2:4]), color, thickness=2)
-        #cv2.imshow("test", image[rect[1]:rect[1]+rect[3], rect[0]:rect[0] + rect[2]])
-        cv2.imshow("test", image)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
+        facerect = cascade.detectMultiScale(image_gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+
+        if len(facerect) > 0:
+
+            for rect in facerect:
+                cv2.rectangle(image, tuple(rect[0:2]), tuple(rect[0:2] + rect[2:4]), color, thickness=2)
+                resize_image = cv2.resize(image[rect[1]:rect[1]+rect[3], rect[0]:rect[0] + rect[2]], image_shape)
+                cv2.imwrite(output_dir + "/白石麻衣_" + str(count_image) + ".jpg", resize_image)
 
 
 class face_Dataset(Dataset):
@@ -65,4 +64,6 @@ class face_Dataset(Dataset):
 
 
 if __name__ == "__main__":
-    test = face_detection("./shiraishi_mai/白石麻衣_137.jpg")
+    image_dir = "./shiraishi_mai"
+    output_dir = "./shiraishi_face"
+    face_detection(image_dir, output_dir)
