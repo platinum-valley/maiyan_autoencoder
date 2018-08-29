1fe3738423b9d7cd9d43e1c4a1d6c8aa083b2f3d
platinum-valley <s163351@edu.tut.ac.jp>
2018-08-29 22:53:35 +0900

    update dataset

--- dataset.py
+++ dataset.py
@@ -4,27 +4,32 @@ import numpy as np
 import torch
 from torch.utils.data import Dataset
 
-class face_Dataset(Dataset):
+class face_train_Dataset(Dataset):
     """
     データセット
 
     """
 
-    def __init__(self, image_dir, transform=None):
+    def __init__(self, image_dir, image_name="shiraishi", transform=None):
         """
         初期化関数
         :param image_dir: 画像のルートディレクトリ
         """
         self.image_dir = image_dir
+        self.image_name = image_name
         self.image_num = len(os.listdir(image_dir))
-        pass
+        self.tranform = transform
 
     def __len__(self):
         """
         データセットの総数を返す
         :return:
         """
-        return len(Dataset)
+        return self.image_num
 
-    def __getitem__(self):
-        pass
+    def __getitem__(self, idx):
+        image_path = os.path.join(self.image_dir, self.image_name + str(idx) + ".jpg")
+        image = cv2.imread(image_path)
+        if self.transform:
+            image = self.tranform(image)
+        return image
